c40a4866777f77bab3ab8956a87c170dbda50ac5
Roman Agaltsev <roman-agalcev@yandex.ru>
2025-02-10 19:59:16 +0300

    feat(api): add specific errors for handler

---
+++ internal/app/avitoshop/api/errors.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+)
+
+var (
+	ErrBadRequest          = &ErrorResponse{StatusCode: 400, Message: "Bad request"}
+	ErrWrongLoginPassword  = &ErrorResponse{StatusCode: 401, Message: "Wrong login/password"}
+	ErrNotEnoughCoins      = &ErrorResponse{StatusCode: 402, Message: "Not enough coins"}
+	ErrLoginIsAlreadyTaken = &ErrorResponse{StatusCode: 409, Message: "Login has already been taken"}
+)
+
+type ErrorResponse struct {
+	Err        error  `json:"-"`
+	StatusCode int    `json:"-"`
+	StatusText string `json:"status_text"`
+	Message    string `json:"message"`
+}
+
+func (e *ErrorResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	render.Status(r, e.StatusCode)
+	return nil
+}
+func ErrorRenderer(err error) *ErrorResponse {
+	return &ErrorResponse{
+		Err:        err,
+		StatusCode: 400,
+		StatusText: "Bad request",
+		Message:    err.Error(),
+	}
+}
+func ServerErrorRenderer(err error) *ErrorResponse {
+	return &ErrorResponse{
+		Err:        err,
+		StatusCode: 500,
+		StatusText: "Internal server error",
+		Message:    err.Error(),
+	}
+}
