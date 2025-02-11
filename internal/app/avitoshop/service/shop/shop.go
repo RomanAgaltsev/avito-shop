2258cc58cd29b547c3507355801fad42d32cf1c3
Roman Agaltsev <roman-agalcev@yandex.ru>
2025-02-11 18:30:57 +0300

    style: run gofmt and goimports

--- internal/app/avitoshop/service/shop/shop.go
+++ internal/app/avitoshop/service/shop/shop.go
@@ -1,68 +1,68 @@
 package shop
 
 import (
-	"context"
+    "context"
 
-	"github.com/RomanAgaltsev/avito-shop/internal/config"
-	"github.com/RomanAgaltsev/avito-shop/internal/model"
+    "github.com/RomanAgaltsev/avito-shop/internal/config"
+    "github.com/RomanAgaltsev/avito-shop/internal/model"
 )
 
 // Service is the user service interface.
 type Service interface {
-	UserRegister(ctx context.Context, user model.User) error
-	UserLogin(ctx context.Context, user model.User) error
-	UserBalance(ctx context.Context, user model.User) error
-	SendCoins(ctx context.Context, fromUser model.User, toUser model.User, amount int) error
-	BuyItem(ctx context.Context, user model.User, item model.InventoryItem) error
-	UserInfo(ctx context.Context, user model.User) (model.Info, error)
+    UserRegister(ctx context.Context, user model.User) error
+    UserLogin(ctx context.Context, user model.User) error
+    UserBalance(ctx context.Context, user model.User) error
+    SendCoins(ctx context.Context, fromUser model.User, toUser model.User, amount int) error
+    BuyItem(ctx context.Context, user model.User, item model.InventoryItem) error
+    UserInfo(ctx context.Context, user model.User) (model.Info, error)
 }
 
 // Repository is the user service repository interface.
 type Repository interface {
-	CreateUser(ctx context.Context, user model.User) error
-	GetUser(ctx context.Context, login string) (model.User, error)
+    CreateUser(ctx context.Context, user model.User) error
+    GetUser(ctx context.Context, login string) (model.User, error)
 }
 
 // NewService creates new user service.
 func NewService(repository Repository, cfg *config.Config) (Service, error) {
-	return &service{
-		repository: repository,
-		cfg:        cfg,
-	}, nil
+    return &service{
+        repository: repository,
+        cfg:        cfg,
+    }, nil
 }
 
 // service is the user service structure.
 type service struct {
-	repository Repository
-	cfg        *config.Config
+    repository Repository
+    cfg        *config.Config
 }
 
 // UserRegister creates new user.
 func (s *service) UserRegister(ctx context.Context, user model.User) error {
-	return nil
+    return nil
 }
 
 // UserLogin logins existed user.
 func (s *service) UserLogin(ctx context.Context, user model.User) error {
-	return nil
+    return nil
 }
 
 // UserBalance creates new user balance.
 func (s *service) UserBalance(ctx context.Context, user model.User) error {
-	return nil
+    return nil
 }
 
 // SendCoins sends given amount of coins from one user to another.
 func (s *service) SendCoins(ctx context.Context, fromUser model.User, toUser model.User, amount int) error {
-	return nil
+    return nil
 }
 
 // BuyItem buys a given inventory item.
 func (s *service) BuyItem(ctx context.Context, user model.User, item model.InventoryItem) error {
-	return nil
+    return nil
 }
 
 // UserInfo returns user info about coins, inventory and transaction history.
 func (s *service) UserInfo(ctx context.Context, user model.User) (model.Info, error) {
-	return model.Info{}, nil
+    return model.Info{}, nil
 }
